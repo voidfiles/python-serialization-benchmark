2798b7c2394e071b6b796d911c830f10ff945755
Alex Kessinger <akessinger@bepress.com>
2016-11-24 09:10:29 -0800

    Try and get docs deploy

--- create_report.sh
+++ create_report.sh
@@ -4,4 +4,5 @@ cat README.md > REPORT.md
 echo "<pre>" >> REPORT.md
 python benchmark.py >> REPORT.md
 echo "</pre>" >> REPORT.md
-python -m markdown REPORT.md > index.html
+mkdir -p out
+python -m markdown REPORT.md > out/index.html
