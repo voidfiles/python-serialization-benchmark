c41695797559c455256db5ff5d0857ed81129048
Alex Kessinger <akessinger@bepress.com>
2016-12-10 23:01:31 -0700

    Bump Strainer version

--- subjects/strain.py
+++ subjects/strain.py
@@ -2,16 +2,16 @@ import strainer
 
 __name__ = 'Strainer'
 
-sub_strainer_serializer = strainer.create_serializer(
+sub_strainer_serializer = strainer.serializer(
   strainer.field('w'),
-  strainer.field('x', to_representation=lambda obj: obj.x + 10),
+  strainer.field('x', attr_getter=lambda obj: obj.x + 10),
   strainer.field('y'),
   strainer.field('z'),
 )
 
-complex_strainer_serializer = strainer.create_serializer(
+complex_strainer_serializer = strainer.serializer(
   strainer.field('foo'),
-  strainer.field('bar', to_representation=lambda obj: obj.bar()),
+  strainer.field('bar', attr_getter=lambda obj: obj.bar()),
   strainer.child('sub', serializer=sub_strainer_serializer),
   strainer.many('subs', serializer=sub_strainer_serializer),
 )
@@ -19,6 +19,6 @@ complex_strainer_serializer = strainer.create_serializer(
 
 def serialization_func(obj, many):
     if many:
-        return [complex_strainer_serializer.to_representation(x) for x in obj]
+        return [complex_strainer_serializer.serialize(x) for x in obj]
     else:
-        return complex_strainer_serializer.to_representation(obj)
+        return complex_strainer_serializer.serialize(obj)
