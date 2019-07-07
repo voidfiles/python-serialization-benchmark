f6ff942461c5c787745f79a100f145d6bcaf98c9
Alex Kessinger <voidfiles@gmail.com>
2019-07-07 11:02:08 -0600

    Update serpy

--- deploy.sh
+++ deploy.sh
@@ -44,11 +44,6 @@ git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy-key.enc -out deploy-key -d
 chmod 600 deploy-key
 eval `ssh-agent -s`
 ssh-add deploy-key
